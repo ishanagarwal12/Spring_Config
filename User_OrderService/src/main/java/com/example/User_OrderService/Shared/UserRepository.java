31c2ee3a837fad06e1eba5508058626bacfea75d
Ishan <ishaan@symboinsurance.com>
2021-12-24 14:00:54 +0530

    Deleted
    git commit -mDeleted

--- User_OrderService/src/main/java/com/example/User_OrderService/Shared/UserRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.example.User_OrderService.Shared;
-//
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//public interface UserRepository extends MongoRepository<UserEntity,Long> {
-//}
--- User_Payment/src/main/java/com/example/User_Payment/Entity/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.User_Payment.Entity;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends MongoRepository<PaymentEntity,String> {
-
-}
