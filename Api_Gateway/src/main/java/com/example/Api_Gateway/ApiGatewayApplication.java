3097d45c4216a41dca28af76a8ba99ffac6b5145
Ishan <ishaan@symboinsurance.com>
2021-12-28 20:14:58 +0530

    Spring Data Auto Configyration property removed

--- Api_Gateway/src/main/java/com/example/Api_Gateway/ApiGatewayApplication.java
+++ Api_Gateway/src/main/java/com/example/Api_Gateway/ApiGatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 @EnableEurekaClient
 public class ApiGatewayApplication {
 
